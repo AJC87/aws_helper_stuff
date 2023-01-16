b51e6f8c73d19bbd44c085856e636e9a299c7997
Ashley Craig <ashleycraig1987@gmail.com>
2023-01-16 21:03:45 +0000

    Build out ECR script

--- scripts/ecr_docker_auth.sh
+++ scripts/ecr_docker_auth.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 # Authenticate my Docker client to an Amazon ECR registry
+
+# TODO: get these from a command
+# Default for now
+readonly REGION='us-east-1'
+readonly ACCOUNT_ID=''
+
+aws ecr get-login-password --region "${REGION}" | \
+    docker login --username AWS --password-stdin "${ACCOUNT_ID}.dkr.ecr.region.amazonaws.com"
