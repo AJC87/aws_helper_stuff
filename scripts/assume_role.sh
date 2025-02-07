29f48fb2a859395a0b9fa699f7bb4800262a5411
Ashley Craig <ashleycraig1987@gmail.com>
2025-02-07 00:48:11 +0000

    update role script

--- scripts/assume_role.sh
+++ scripts/assume_role.sh
@@ -8,6 +8,10 @@ readonly ACCOUNT_ID=''
 readonly ROLE_NAME=''
 readonly ROLE_SESSION_NAME='testing'
 
+# TODO: get the credentials from the command and export them, also use a profile for this
 aws sts assume-role \
     --role-arn "arn:aws:iam::${ACCOUNT_ID}:role/${ROLE_NAME}" \
     --role-session-name "${ROLE_SESSION_NAME}"
+
+# Print the role to confirm that we have switched to it
+aws sts get-caller-identity
