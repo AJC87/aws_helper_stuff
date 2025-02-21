faef6878a2d2055654c62244e692cc5c658af868
Ashley Craig <ashleycraig1987@gmail.com>
2025-02-21 23:56:31 +0000

    More ECR stuff

--- cdk/lib/components/helpers.ts
+++ cdk/lib/components/helpers.ts
@@ -1,7 +1,30 @@
 import { Stack } from 'aws-cdk-lib';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { IRuleTarget } from 'aws-cdk-lib/aws-events';
+
+declare const target: IRuleTarget;
 
 export function createECRRepository(this: Stack): void {
     // TODO: finish me
-    const repository = new ecr.Repository(this, 'my_repository');
+
+    // Get user from elsewhere
+    // const user = new iam.User(this, 'my_user');
+    // ecr.AuthorizationToken.grantRead(user);
+    // ecr.PublicGalleryAuthorizationToken.grantRead(user);
+
+    // Get role from elsewhere, set up CodeBuild
+    // const role = new iam.Role(this, 'my_role', {
+    //     assumedBy: new iam.ServicePrincipal('codebuild.amazonaws.com'),
+    //   });
+
+    const repository = new ecr.Repository(this, 'my_repository', {
+        // False for now to save money
+        imageScanOnPush: false,
+      });
+
+    // repository.grantPush(role);
+
+    // repository.onImageScanCompleted('ImageScanComplete')
+    //     .addTarget(target);
 }
